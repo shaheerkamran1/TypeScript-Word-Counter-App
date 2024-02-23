79c961fa2495ccd8ce1f3a73914d4f35317e8e33
Muhammad Shaheer Kamran <112251353+shaheerkamran1@users.noreply.github.com>
2024-02-23 05:05:06 +0500

    Add files via upload

---
+++ cli_wordCounter_piaic_shaheer/README.md
@@ -0,0 +1,49 @@
+# TypeScript Word Counter Application using chalk and inquirer - Muhammad Shaheer Kamran
+
+A cli Word Counter Application  designed witht the help of chalk and developed using TypeScript. 
+
+This is a console based application which allows the user to input his/her paragraph or sentence and the system starts. Which then provied the user with total number of words and total number of letters.
+
+
+This program is made for the sole purpose of practice and showing skills based upon TypeScript language.
+
+
+# Download the Package
+
+```
+npm i cli_wordCounter_piaic_shaheer
+```
+
+# Run the Word Counter App on your terminal
+
+```
+npx cli_wordCounter_piaic_shaheer
+```
+
+# Packages
+
+[chalk](https://github.com/chalk/chalk) | 
+[inquirer](https://github.com/SBoudrias/Inquirer.js)
+
+# Inquirer
+```
+npm i inquirer
+```
+
+
+# Chalk
+```
+npm i chalk
+```
+
+# Dev Dependencies
+```
+npm i @types/inquirer -D
+npm i @types/chalk -D
+```
+
+# Node Modules
+```
+npm i node
+npm i @types/node
+```
---
+++ cli_wordCounter_piaic_shaheer/bin/counter.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+/* The user will enter a english paragraph
+and all that is needed is to just to implement
+counting characters and words without whitespaces.
+Create a GitHub repository for the project
+and submit its URL in the project submission form. */
+const green = chalk.greenBright.bold.underline;
+const red = chalk.redBright.bold;
+const yellow = chalk.yellowBright.bold;
+const blue = chalk.blueBright;
+const whites = chalk.whiteBright.bold;
+console.log("\n\n");
+console.log("\n\n");
+console.log(green(" ██╗  ██╗ ███  ██╗ ████ ██╗ ███  ███╗         █████╗  █████╗  ██╗   ██╗ ███╗ ░██╗ ████████╗ ███████╗ ██████╗ "));
+console.log(green(" ██║  ██╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗    ██╔══██╗ ██╔══██╗ ██║   ██║ ████╗░██║ ╚══██╔══╝ ██╔════╝ ██╔══██╗"));
+console.log(green(" ╚██╗████╗██╔╝ ██║  ██║ ██████╔╝ ██║  ██║    ██║  ╚═╝ ██║  ██║ ██║   ██║ ██╔██╗██║    ██║    █████╗   ██████╔╝"));
+console.log(green("  ████╔═████║  ██║  ██║ ██╔══██╗ ██║  ██║    ██║  ██╗ ██║  ██║ ██║   ██║ ██║╚████║    ██║    ██╔══╝   ██╔══██╗"));
+console.log(green("  ╚██╔╝ ╚██╔╝  ╚█████╔╝ ██║  ██║ ██████╔╝    ╚█████╔╝ ╚█████╔╝ ╚██████╔╝ ██║ ╚███║    ██║    ███████╗ ██║  ██║"));
+console.log(green("   ╚═╝   ╚═╝    ╚════╝  ╚═╝  ╚═╝ ╚═════╝      ╚════╝   ╚════╝   ╚═════╝  ╚═╝  ╚══╝    ╚═╝    ╚══════╝ ╚═╝  ╚═╝"));
+console.log();
+console.log(blue(`Ｄｅｖｅｌｏｐｅｄ  ｗｉｔｈ ${red('❤')}  ｂｙ  Ｓｈａｈｅｅｒ`));
+console.log();
+;
+async function counts() {
+    console.log();
+    let paragraph1 = await inquirer.prompt([
+        {
+            name: "user",
+            type: "string",
+            message: green("Enter a paragraph or sentence: "),
+        }
+    ]);
+    let paragraph = paragraph1.user;
+    let countLetter = 0;
+    for (let counter = 0; counter < paragraph.length; counter++) {
+        if (paragraph[counter] == " ") {
+            countLetter += 0;
+        }
+        else if (paragraph[counter] != " ") {
+            countLetter += 1;
+        }
+    }
+    const words = paragraph.split(" ");
+    console.log(red(`\nYour number of words: ${words.length}`));
+    console.log(red(`\nYour Number of letters: ${countLetter}\n`));
+}
+await counts();
+do {
+    var start = await inquirer.prompt([{
+            name: "again",
+            type: "list",
+            choices: ["Yes", "No"],
+            message: yellow("Start Again?\n")
+        }]);
+    if (start.again == "Yes") {
+        await counts();
+    }
+    else if (start.again == "No") {
+        console.log(green("\nThanks for using Word Counter\n"));
+        break;
+    }
+} while (start.again != "No");
